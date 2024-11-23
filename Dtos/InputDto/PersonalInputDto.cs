e50473b0ef4710ff0833a3fb2bd55eb4e2438204
RodrigoFroes <craftland262@gmail.com>
2024-11-23 13:02:46 -0300

    Feat: Add Personal Dto

--- Dtos/InputDto/PersonalInputDto.cs
+++ Dtos/InputDto/PersonalInputDto.cs
@@ -1,28 +1,16 @@
-﻿using BuscaPersonalApi.Entities;
-using System.ComponentModel.DataAnnotations;
-
-namespace BuscaPersonalApi.Dtos.InputDto
+﻿namespace BuscaPersonalApi.Dtos.InputDto
 {
     public class PersonalInputDto
     {
-        [Required(ErrorMessage = "O campo Id é obrigatório.")]
         public int Id { get; set; }
-        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
-        public string Nome { get; set; }
-        [Required(ErrorMessage = "O campo CPF é obrigatório.")]
-        public string CPF { get; set; }
-        [Required(ErrorMessage = "O campo CREF é obrigatório.")]
-        public string CREF { get; set; }
-        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
-        public string Telefone { get; set; }
-        [Required(ErrorMessage = "O campo Data de Nascimento é obrigatório.")]
-        public DateOnly DataNascimento { get; set; }
-        [Required(ErrorMessage = "O campo Especialidade é obrigatório.")]
-        public string Especialidade { get; set; }
-        [Required(ErrorMessage = "O campo Email é obrigatório.")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
-        public string Senha { get; set; }
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public string? CREF { get; set; }       
+        public string? Telefone { get; set; }       
+        public DateOnly? DataNascimento { get; set; }
+        public string? Especialidade { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
     }
 
 }
