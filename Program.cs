b6112f9015165646e7a3c4ec70d6645b9d6fef4f
RodrigoFroes <craftland262@gmail.com>
2024-11-23 13:04:07 -0300

    Feat: Add Configuracao

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
+using BuscaPersonalApi.AutoMapper;
 using BuscaPersonalApi.Data;
+using BuscaPersonalApi.Repositories;
+using BuscaPersonalApi.Repositories.Interface;
+using BuscaPersonalApi.Services;
+using BuscaPersonalApi.Utils;
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 Env.Load();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<PersonalService>();
+builder.Services.AddScoped<ValidarUtils>();
+builder.Services.AddScoped<IPersonal, PersonalRepository>();
+builder.Services.AddAutoMapper(typeof(AutoMapperPofile));
 builder.Services.AddSwaggerGen();
+
 
 string? connectionString = Environment.GetEnvironmentVariable("URL");
 
